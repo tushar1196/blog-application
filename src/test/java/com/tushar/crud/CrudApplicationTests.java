b76836fba960f140c4869cf33f95a3dcbd3cfe7f
Tushar Chavhan <tusharchavhan111196@gmail.com>
2021-06-12 13:20:44 +0530

    Initial Commit

---
+++ src/test/java/com/tushar/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tushar.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
