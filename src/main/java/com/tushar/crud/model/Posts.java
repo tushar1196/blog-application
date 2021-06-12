d597623e492f0146ebf01f96f8f1bcd93bb52195
Tushar Chavhan <tusharchavhan111196@gmail.com>
2021-06-12 14:17:39 +0530

    Post Entity Ready

---
+++ src/main/java/com/tushar/crud/model/Posts.java
@@ -0,0 +1,107 @@
+package com.tushar.crud.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Entity
+public class Posts {
+
+    @Id
+    private int id;
+    private String title;
+    private String excerpt;
+    private String content;
+    private String author;
+    private Timestamp published_at;
+    private boolean is_published;
+    private Timestamp created_at;
+    private Timestamp updated_at;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getExcerpt() {
+        return excerpt;
+    }
+
+    public void setExcerpt(String excerpt) {
+        this.excerpt = excerpt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Timestamp getPublished_at() {
+        return published_at;
+    }
+
+    public void setPublished_at(Timestamp published_at) {
+        this.published_at = published_at;
+    }
+
+    public boolean isIs_published() {
+        return is_published;
+    }
+
+    public void setIs_published(boolean is_published) {
+        this.is_published = is_published;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    @Override
+    public String toString() {
+        return "Posts{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", excerpt='" + excerpt + '\'' +
+                ", content='" + content + '\'' +
+                ", author='" + author + '\'' +
+                ", published_at=" + published_at +
+                ", is_published=" + is_published +
+                ", created_at=" + created_at +
+                ", updated_at=" + updated_at +
+                '}';
+    }
+}
