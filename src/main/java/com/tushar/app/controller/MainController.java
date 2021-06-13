6461ebe49adec55539dd34104a5d9bd1a022c4fb
Tushar Chavhan <tusharchavhan111196@gmail.com>
2021-06-14 02:56:59 +0530

    html ready

--- src/main/java/com/tushar/app/controller/MainController.java
+++ src/main/java/com/tushar/app/controller/MainController.java
@@ -1,9 +1,13 @@
 package com.tushar.app.controller;
 
+import com.tushar.app.model.Posts;
 import com.tushar.app.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -19,13 +23,13 @@ public class MainController {
     @Autowired
     UserRepo userRepo;
 
-    @RequestMapping("/blogssdashboard")
-    public String getBlogs() {
+    @RequestMapping("/blogsdashboard")
+    public String getBlogs(Model model) {
         System.out.println("in blogs");
-        
-
-
-        return "blogdashboard";
+        List<Posts> posts = postsRepo.findAll();
+        System.out.println(posts);
+//        model.addAttribute("posts",posts);
+        return "blogsdashboard";
     }
 
 
