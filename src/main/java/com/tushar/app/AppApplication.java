e85f194590be0b78c975ade5dd8263316af4f034
Tushar Chavhan <tusharchavhan111196@gmail.com>
2021-07-08 18:32:14 +0530

    desigining user dashboard

--- src/main/java/com/tushar/app/AppApplication.java
+++ src/main/java/com/tushar/app/AppApplication.java
@@ -1,9 +1,8 @@
 package com.tushar.app;
 
-import com.tushar.app.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
 public class AppApplication {
@@ -11,5 +10,4 @@ public class AppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
-
 }
